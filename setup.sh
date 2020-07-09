5faa38c73d0bbe2c864994f94ad2c461dbcf3e3c
ayokinya <jkang@c2r5s5.42seoul.kr>
2020-07-09 11:38:25 +0900

    nginx on kubernetes done

--- setup.sh
+++ setup.sh
@@ -5,28 +5,32 @@ minikube config set vm-driver virtualbox
 minikube start --extra-config=apiserver.service-node-port-range=1-35000
 echo "Enabling addons..."
 
+minikube addons enable metrics-server
 minikube addons enable dashboard
-minikube addons enable ingress
+
 echo "Launching dashboard..."
 minikube dashboard &
 
-echo "Eval..."
+echo "Eval... to link MINIKUBE AND DOCKER"
 eval $(minikube docker-env)
 
 #IP=$(minikube ip)
-IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+export IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 printf "Minikube IP: ${IP}\n"
+
+echo ${IP} > /nginx/ip.txt
+echo ${IP} > /ftps/ip.txt
 
 echo "Building images..."
 docker build -t service_nginx ./nginx
 
-
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl apply -f config.yaml
+kubectl apply -f metalLB_config.yaml
 
 echo "Creating pods and services..."
 kubectl create -f nginx.yaml
 
 echo "Opening the network in your browser"
 #open http://$IP
+minikube service list
