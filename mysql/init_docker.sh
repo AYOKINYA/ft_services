7191ea7f3dde7d5c13b665360b3da422bf3426e8
ayokinya <jkang@c2r5s5.42seoul.kr>
2020-07-18 16:28:50 +0900

    livenessProbe added

--- mysql/init_docker.sh
+++ mysql/init_docker.sh
@@ -2,4 +2,8 @@
 
 mysql_install_db --user=root
 mysqld --user=root --bootstrap < /tmp/init_mysql.sh
+sleep 5
+IP=$(cat /ip.txt)
+sed -i "s/192.168.99.152/$IP/g" /etc/wordpress/wp-config.php
+mysql -u root -p wordpress < wordpress.sql
 mysqld --user=root
