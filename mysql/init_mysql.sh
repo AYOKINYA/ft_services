61cc55c3cb576ed8aac3d17ea325163574bed125
ayokinya <jkang@c2r5s5.42seoul.kr>
2020-07-18 14:43:44 +0900

    finally phpmyadmin login success!

--- mysql/init_mysql.sh
+++ mysql/init_mysql.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-until mysql
-do
-    echo "NO_UP"
-done
-
-mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root -e "CREATE USER 'wpuser'@'%' IDENTIFIED BY '123456789';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "update mysql.user set plugin = 'mysql_native_password' where user='root';"
+FLUSH PRIVILEGES;
+CREATE DATABASE wordpress;
+CREATE USER 'wpuser'@'%' IDENTIFIED BY '123456789';
+GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+#update mysql.user set plugin = 'mysql_native_password' where user='root';
