a159f571551bddecaae9c39c2f00e49bfab13a1b
ayokinya <jkang@c2r5s5.42seoul.kr>
2020-07-20 09:52:43 +0900

    wordpress sql dump file is inserted successfully

--- mysql/init_docker.sh
+++ mysql/init_docker.sh
@@ -4,6 +4,6 @@ mysql_install_db --user=root
 mysqld --user=root --bootstrap < /tmp/init_mysql.sh
 sleep 5
 IP=$(cat /ip.txt)
-sed -i "s/192.168.99.152/$IP/g" /etc/wordpress/wp-config.php
-mysql -u root -p wordpress < wordpress.sql
+sed -i "s/192.168.99.152/$IP/g" /tmp/wordpress.sql
+mysql wordpress < /tmp/wordpress.sql
 mysqld --user=root
