5ae6c82ecb8fa04f339e55eaa9a1330a1cf8702a
ayokinya <jkang@c2r5s5.42seoul.kr>
2020-07-20 16:16:26 +0900

    dashboards are created respectively

--- mysql/init_docker.sh
+++ mysql/init_docker.sh
@@ -2,8 +2,8 @@
 
 mysql_install_db --user=root
 mysqld --user=root --bootstrap < /tmp/init_mysql.sh
-sleep 5
 IP=$(cat /ip.txt)
 sed -i "s/192.168.99.152/$IP/g" /tmp/wordpress.sql
-mysql wordpress < /tmp/wordpress.sql
+mysql -u root wordpress < /tmp/wordpress.sql
+sleep 5
 mysqld --user=root
